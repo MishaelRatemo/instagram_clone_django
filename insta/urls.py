351c9d58ff5a646c7d2db77e597a378415e73e60
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-10 06:53:43 +0300

    Added urls for views

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,12 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.insta, name='index' ),
     path('logout', views.logout, name='logout'),
+    path('post', views.post_data , name='post'),
+    path('explore', views.explore , name='explore'),
+    path('comment/<int:id>/', views.comments, name='comment'),
+    path('like/<int:id>/', views.likes, name='like')
+    
 ]
