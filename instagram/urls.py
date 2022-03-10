2c2192ba24bd1d54646481b1a2a8a0a337c73ccb
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-10 06:56:49 +0300

    Added static setting to allow loading of static files and media

--- instagram/urls.py
+++ instagram/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
