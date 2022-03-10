064670c4d05523e45a63667fa04e7725c44bdfb7
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-10 06:53:07 +0300

    Added post form

---
+++ insta/forms.py
@@ -0,0 +1,13 @@
+from .models import Profile,Post
+from django import forms
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['author',
+                  'date_posted',
+                  
+                  ]
+        
+        
